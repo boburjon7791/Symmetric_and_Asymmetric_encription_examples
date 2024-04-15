c73470f2a08f0f5a8d188b53ad6a4388bec16ebd
Boburjon <soliyevboburjon95@gmail.com>
2024-04-15 06:43:02 +0500

    Symmetric And Asymmetric Encryption Examples. I have customised

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -5,17 +5,24 @@ public class Main {
         String value="Hello world";
         System.out.println("value = " + value);
         System.out.println("Symmetric");
+
         AbstractEncryption<String> symmetric = AbstractEncryption.ofSymmetric();
         String encrypted = symmetric.encrypt(value);
-        System.out.println("encrypted = " + encrypted);
         String decrypted = symmetric.decrypt(encrypted);
+
+
+
+        System.out.println("encrypted = " + encrypted);
         System.out.println("decrypted = " + decrypted);
+
         System.out.println("=========== ");
         System.out.println("Asymmetric");
+
         AbstractEncryption<byte[]> asymmetric = AbstractEncryption.ofAsymmetric();
         byte[] encrypted1 = asymmetric.encrypt(value);
-        System.out.println("encrypted = " + new String(encrypted1));
         String decrypted1 = asymmetric.decrypt(encrypted1);
+
+        System.out.println("encrypted = " + new String(encrypted1));
         System.out.println("decrypted = " + decrypted1);
     }
 }
